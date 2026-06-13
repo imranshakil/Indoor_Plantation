46e0dceb0968bb3a9bffc71cf212283a5e52e1b3
Sarwar Oyon <sarwar@cistechltd.com>
2026-06-13 11:21:31 +0600

    Revise soil and water sensor calibration constants
    Updated soil sensor calibration values and thresholds.

--- src/config.h
+++ src/config.h
@@ -28,8 +28,17 @@ constexpr uint32_t GROW_LED_PWM_FREQUENCY = 1000;
 constexpr uint8_t GROW_LED_PWM_RESOLUTION = 8;
 
 // ===================== THRESHOLDS =====================
-constexpr int SOIL_LOW_THRESHOLD = 2000;
-constexpr int WATER_LOW_PERCENT = 10;
+/ Soil Sensor 1 Calibration
+constexpr int SOIL1_RAW_DRY = 3450;
+constexpr int SOIL1_RAW_WET = 1760;
+
+// Soil Sensor 2 Calibration
+constexpr int SOIL2_RAW_DRY = 2870;
+constexpr int SOIL2_RAW_WET = 1020;
+
+// Moisture Alert Threshold
+constexpr int SOIL_LOW_PERCENT = 30;
+constexpr int WATER_LOW_PERCENT = 20;
 constexpr float TEMP_MAX_C = 35.0;
 constexpr float HUMIDITY_MAX_PERCENT = 80.0;
 
@@ -41,17 +50,16 @@ constexpr unsigned long WIFI_CONNECT_TIMEOUT_MS = 20000;
 constexpr unsigned long WIFI_CONNECT_POLL_MS = 500;
 
 // ===================== WATER SENSOR CALIBRATION =====================
-constexpr int WATER_RAW_EMPTY = 0;
-constexpr int WATER_RAW_MIDDLE = 1510;
-constexpr int WATER_RAW_FULL = 1830;
+constexpr int WATER_RAW_EMPTY = 160;
+constexpr int WATER_RAW_FULL  = 330;
 
 // ===================== API =====================
 constexpr uint16_t API_PORT = 80;
 
 
 // ===================== CLOUD / DJANGO API For Backups=====================
-constexpr const char* CLOUD_SENSOR_LOG_URL = "https://yourdomain.com/api/plantation/sensor-log";
-constexpr const char* CLOUD_EVENT_LOG_URL  = "https://yourdomain.com/api/plantation/event-log";
+constexpr const char* CLOUD_SENSOR_LOG_URL = "http://192.168.68.148:8000/api/plantation/sensor-log";
+constexpr const char* CLOUD_EVENT_LOG_URL  = "http://192.168.68.148:8000/api/plantation/event-log";
 constexpr const char* API_TOKEN = "CHANGE_THIS_SECRET_TOKEN";
 
 constexpr unsigned long CLOUD_SENSOR_SEND_INTERVAL_MS = 300000; // 5 minute
